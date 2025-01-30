2e35a9c4eb9af8d9deccef1e6a7890ede5a9c452
Rahul Tandon <jesuisrahult@gmail.com>
2025-01-30 16:30:59 +0530

    edit Jenkinsfile

--- 04/hello.py
+++ 04/hello.py
@@ -1,1 +1,11 @@
 print("this verifies that hello .py can run from jenkins")
+
+if __name__ == '__main__':
+    '''
+    Sample usage:
+    <filename.py>  path_to_master_csv s3_bucket_uri sqs_url
+    name 
+    '''
+    # step 1: split the csv into (what we will call) chunks
+    # step 2: upload each chunk to S3. Store s3 uri of each chunk.
+    # step 3: for each chunk_s3_uri, send a message to SQS
