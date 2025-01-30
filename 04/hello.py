17aa6a7ab8135903bf9d5ab70cdb10c4c3de59bb
Rahul Tandon <jesuisrahult@gmail.com>
2025-01-30 17:49:11 +0530

    edit Jenkinsfile

--- 04/hello.py
+++ 04/hello.py
@@ -1,4 +1,5 @@
 import sys
+import boto3
 
 if __name__ == '__main__':
     '''
@@ -18,6 +19,18 @@ if __name__ == '__main__':
     print('s3_bucket', s3_bucket)
     print('sqs_url', sqs_url)
      
+    # ==============================================================================
+    # step 0: push JMX and CSV to S3 for ability to recreate test later if required
+    # ==============================================================================
+    s3_key_of_master_csv=f"{test_unique_id}/master_data.csv"
+    s3_key_of_jmx=f"{test_unique_id}/test_plan.jmx"
+    
+    s3 = boto3.client('s3')
+    s3.upload_file(local_path_to_csv, s3_bucket, s3_key_of_master_csv)
+    s3.upload_file(local_path_to_jmx, s3_bucket, s3_key_of_jmx)
+    print("Completed Step 0: Upload master CSV & JMX to S3")
+
+    # ==============================================================================
     # step 1: split the csv into (what we will call) chunks
     # step 2: upload each chunk to S3. Store s3 uri of each chunk.
     # step 3: for each chunk_s3_uri, send a message to SQS
